939c1cae0926897ad961c5098643a309374759b3
Matthew Woodard <mat.james.woodard@gmail.com>
2023-10-02 14:48:50 -0700

    Refactor the call_api method to use a construct_path method
    Refactor Baseline methods to use the set_options method instead of merging option hashes

--- lib/baseline_stats.rb
+++ lib/baseline_stats.rb
@@ -7,75 +7,46 @@ require 'baseline_request'
 class Baseline
   include BaselineEndpoints
 
-  def self.api_request(endpoint, options = {})
-    options = options.with_indifferent_access
-    error_array = []
-    url_info = ENDPOINTS[endpoint]
-    url = url_info[:url].dup
-    url_info[:path_params].each do |path_key, value|
-      case value[:type]
-      when "bool"
-        path_value = value[options[path_key].to_s] || value[value[:default].to_s]
-      else
-        path_value = options[path_key] || value[:default]
-      end
-      error_array << "Missing required path parameter: #{path_key}" if value[:required] && path_value.blank?
-      path_value = path_value.to_s
-      path_value = "/" + path_value if path_value.present? && value[:leading_slash]
-      url.sub! "{#{path_key}}", path_value
-    end
-
-    query = options.slice(*url_info[:query_params])
-    url_info[:required_params].each do |req_group|
-      error_array << "Missing one of the following required parameters: #{req_group.to_s}" unless req_group.any? {query.keys.include? _1 }
-    end
-    return error_array unless error_array.empty?
-    HTTParty.get(url, {:query => query})
-  end
 
   MLB_LEAGUE_IDS = [103, 104].freeze
   def self.team_list(options = {"leagueIds" => MLB_LEAGUE_IDS})
     BaselineRequest.new('teams', options).call_api
   end
 
-  def self.team_player_list(team_id, options = {})
-    BaselineRequest.new('team_roster', {"teamId": team_id}.merge(options)).call_api
+  def self.team_player_list(options = {})
+    BaselineRequest.new('team_roster', options).call_api
   end
 
-  def self.all_player_list(season = Time.now.year, options = {})
-    BaselineRequest.new("sports_players", {"sportId" => 1, "season" => season}.merge(options)).call_api
+  def self.all_player_list(options = {})
+    BaselineRequest.new("sports_players", options)
+                   .set_option("sportId", 1, false)
+                   .set_option("season", Time.now.year, false).call_api
   end
 
-  def self.schedule_current_year(start_date = nil, end_date = nil, options = {})
-    default_year = DateTime.now.year.to_s
-    start_date_string = start_date&.strftime("%Y-%m-%d")
-    end_date_string = end_date&.strftime("%Y-%m-%d")
-    options["startDate"] = start_date_string || "#{default_year}-03-29"
-    options["endDate"] = end_date_string || "#{default_year}-10-02"
-    api_response = BaselineRequest.new('schedule', {"sportId" => 1 }.merge(options)).call_api
-    games = []
-    api_response["dates"].each do |day|
-      games += day["games"]
+  def self.schedule_year(year, options = {})
+    request = BaselineRequest.new('schedule', options)
+                             .set_option("sportId", 1, false)
+                             .set_option("startDate", "#{year}-03-29")
+                             .set_option("endDate", "#{year}-10-02")
+    request.call_api["dates"].inject([]) do |result, day|
+      result += day["games"]
+      result
     end
-    games
   end
 
   def self.game_at_bats(game_id, options = {})
-    api_response = BaselineRequest.new('game_playByPlay', {"gamePk": game_id}.merge(options)).call_api
+    api_response = BaselineRequest.new('game_playByPlay', options).set_option("gamePk", game_id).call_api
     api_response['allPlays'].filter{_1["result"]["type"] == "atBat"}
   end
 
   def self.player_stats(player_id, options = {})
-    BaselineRequest.new('person_stats', {"personId": player_id, "stats": "season"}.merge(options)).call_api
+    BaselineRequest.new('person_stats', options)
+                   .set_option("personId", player_id)
+                   .set_option("stats", "season", false).call_api
   end
 
   def self.player_info(player_id, options = {})
-    BaselineRequest.new('person', {"personId": player_id}.merge(options))
-    Baseline.api_request("person", {"personId": player_id}.merge(options))["people"].first
-  end
-
-  def self.endpoint_info(endpoint)
-    ENDPOINTS[endpoint]
+    BaselineRequest.new('person', options).set_option("personId", player_id).call_api
   end
 
   def self.test_endpoints
